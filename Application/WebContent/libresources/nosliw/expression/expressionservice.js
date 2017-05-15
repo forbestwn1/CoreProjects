06f174832a5e0416615f04a2ec1d88c4026fd7f0
Ning Wang <ning.c.wang@ericsson.com>
2017-05-15 16:28:48 -0400

    111111111

--- Application/WebContent/libresources/nosliw/expression/expressionservice.js
+++ Application/WebContent/libresources/nosliw/expression/expressionservice.js
@@ -25,7 +25,7 @@ var loc_executeExpression = function(expression, variables, handlers){
 	executeExpressionRequest.addRequet(convertVarsRequest);
 	
 	//execute operand
-	var executeOperandRequest = loc_buildExecuteOperand(expression.operand, variablesData, {
+	var executeOperandRequest = loc_buildExecuteOperandRequest(expression.operand, variablesData, {
 		success : function(operandResult, requestInfo){
 			
 		}
@@ -55,17 +55,146 @@ var loc_buildConvertVarsTask = function(varsData, varsConverter, handlers){
 
 //convert individual var
 var loc_buildConvertTask = function(data, varConverter, handlers){
+	var dataType = data.dataType;
+	var converter = varConverter[dataType];
 	
+	if(converter==undefined){
+		
+	}
+	else{
+		var out = createServiceRequestInfoService(null, handlers);
+		
+		var converterIds;
+		var resourceTask = resourceManager.createGetResourcesTask(converterIds, {
+			success : function(resources, requestInfo){
+				var input = data;
+				for(var i in converterIds){
+					var converterId = converterIds[i];
+					var resource = resources[converterId];
+					input = resource.data[input];
+				}
+				return input;
+			}
+		});
+		
+		out.setDependentService(resourceTask);
+		return out;
+	}
+	
+	return resourceManager.createGetResourcesTask();
 };
 
-
-var loc_buildExecuteOperand = function(operand, variables, handlers){
-	
+var loc_buildExecuteOperandRequest = function(operand, variables, handlers){
+	var out;
+	var operandType = operand.type;
+	switch(operandType){
+	case CONSTANT:
+		out = createServiceRequestInfoService(undefined, loc_calConstant, handlers);
+		break;
+	case VARIABLE: 
+		out = createServiceRequestInfoService(undefined, loc_calVariable, handlers);
+	    break;
+	case OPERATION:
+		out = loc_buildExecuteOperationOperandRequest(operand, variables, handlers);
+		break;
+	case REFERENCE:
+		out = expressionService.buildExecuteExpressionRequest(operand.expression, variables, handlers);
+		break;
+	}
 };
 
-var loc_buildExecuteOperation = function(dataTypeId, operation, requestInfo){
+var loc_buildExecuteOperationOperandRequest = function(operationOperand, variables, handlers, requestInfo){
+	var out = createServiceRequestInfoSequence();
 
+	//cal all parms data and base data
+	var parmsOperand = operationOperand.parms;
+	var baseOperand = operationOperand.baseOperand;
+	
+	var parmsData = {};
+	var baseData;
+	
+	var parmsOperandRequest = createServiceRequestInfoSequenceSet(undefined, {
+		success : function(data, requestInfo){
+			
+		}
+	});
+	_.each(parmsOperand, function(parmOperand, parmName, list){
+		var parmOperandRequest = loc_buildExecuteOperandRequest(parmOperand, variables, {
+			success : function(result, requestInfo){
+				parmsData[parmName] = result;
+			}
+		});
+		parmsOperandRequest.add(parmOperandRequest);
+	}, this);
+	
+	var baseOperandRequest = loc_buildExecuteOperandRequest(baseOperand, variables, {
+		success : function(result, requestInfo){
+			baseData = result;
+		}
+	});
+	parmsOperandRequest.add(baseOperandRequest);
+	out.addRequest(parmsOperandRequest);
+	
+	
+	//figure out operation and converter
+	var operationDiscoveryRequest = resourceManager.createOperationDiscoveryRequest(operationName, parms, baseData, {
+		success : function(operationDiscovery){
+			operationOperand.dataTypeId = operationDiscovery.dataTypeId;
+			for(var parmName in operationOperand.parmNames){
+				var parmConverters = operationOperand.parmsConverters[parmName];
+				for(var dataTypeId in parmVerters){
+					operationOperand.parmConverters[parmName][dataTypeId] = parmVerters[dataTypeId];
+				}
+			}
+		}
+	}, requestInfo);
+	out.addRequest(operationDiscoveryRequest);
+	
+	//prepare parm and base data
+	var operationParms = {};
+	var operationBaseData;
+	
+	var parmsConvertRequest = createServiceRequestInfoSequenceSet(undefined, {
+		success : function(data, requestInfo){
+			
+		}
+	});
+	_.each(parmsData, function(parmData, parmName, list){
+		var parmConvertRequest = loc_buildConvertTask(parmData, operationOperand.parmConverters[parmName], {
+			success : function(parmData, requestInfo){
+				operationParms[parmName] = parmData;
+			}
+		});
+		parmsConvertRequest.add(parmOperandRequest);
+	}, this);
+	out.addRequest(parmsConvertRequest);
+	
+	//cal operation
+	var operationRequest = loc_buildExecuteOperationRequest(operationOperand.dataTypeId, operationOperand.operation, operationParms, operationBaseData);
+	out.addRequest(operationRequest);
+	
+	return out;
+}
+
+var loc_buildExecuteOperationRequest = function(dataTypeId, operation, parms, requestInfo){
+	var operationId;
+	var resourceIds;
+	var resourceTask = resourceManager.createGetResourcesTask(resourceIds, {
+		success : function(resources, requestInfo){
+			var operation = resources[operationId];
+			var result = operation.operate(parms);
+			return result
+		}
+	});
 };	
+
+var loc_calConstant = function(requestInfo){
+	return operand.data;
+}
+
+var loc_calVariable = function(requestInfo){
+	return operand.data;
+}
 
 
 
