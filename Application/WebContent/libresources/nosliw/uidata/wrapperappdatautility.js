75336e3d9063c7f62315ac636e2c0bf7f0bc4cf3
Ning Wang <ning.c.wang@ericsson.com>
2017-12-13 23:43:55 -0500

    1111111

--- Application/WebContent/libresources/nosliw/uidata/wrapperappdatautility.js
+++ Application/WebContent/libresources/nosliw/uidata/wrapperappdatautility.js
@@ -10,39 +10,50 @@ var node_parseSegment;
 var node_createServiceRequestInfoSequence;
 var node_ServiceInfo;
 var node_OperationParm;
+var node_dataUtility;
 	
 //*******************************************   Start Node Definition  ************************************** 	
-var node_utility = {
+var node_utility = function(){
 	
-	getGetChildAppDataRequest : function(appData, path, handlers, requester_parent){
-		var out = node_createServiceRequestInfoSequence(new node_ServiceInfo("GetChildAppData", {"appData":appData, "path":path}), handlers, requester_parent);
+	var loc_getChildAppDataRequest = function(opData, segs, i){
+		var operationParms = [];
 		
-		var segs = node_parseSegment(path);
+		var pathSeg = segs.next();
+		i++;
 		var size = segs.getSegmentSize();
-		var opData = appData;
-		for(var i=0; i<size; i++){
-			var pathSeg = segs.next();
-			var operationParms = [];
-			
-			operationParms.push(new node_OperationParm(opData, "base"));
-			operationParms.push(new node_OperationParm(pathSeg, "name"));
-			
-			var childRequest = nosliw.runtime.getExpressionService().getExecuteOperationRequest(
-					opData.dataTypeId, 
-					node_COMMONCONSTANT.DATAOPERATION_COMPLEX_GETCHILDDATA, 
-					operationParms, 
-					{
-						success : function(request, childData){
-							opData = childData; 
-						}
-					});
-			out.addRequest(childRequest);
-		}
-		
-		return out;
-	},
 
-};
+		operationParms.push(new node_OperationParm(opData, "base"));
+		operationParms.push(new node_OperationParm({
+			dataTypeId: "test.string;1.0.0",
+			value : pathSeg
+		}, "name"));
+
+		var request = nosliw.runtime.getExpressionService().getExecuteOperationRequest(
+				opData.dataTypeId, 
+				node_COMMONCONSTANT.DATAOPERATION_COMPLEX_GETCHILDDATA, 
+				operationParms, {
+					success : function(request, childData){
+						if(i>=size)  return node_dataUtility.createDataOfAppData(childData);
+						if(childData==undefined)  return;
+						return loc_getChildAppDataRequest(childData, segs, i);
+					}
+				});
+		return request;
+	};
+		
+	var loc_out = {
+
+			getGetChildAppDataRequest : function(appData, path, handlers, requester_parent){
+				var out = node_createServiceRequestInfoSequence(new node_ServiceInfo("GetChildAppData", {"appData":appData, "path":path}), handlers, requester_parent);
+				out.addRequest(loc_getChildAppDataRequest(appData, node_parseSegment(path), 0));
+				return out;
+			},
+	};	
+	
+	
+	return loc_out;
+
+}();
 
 //*******************************************   End Node Definition  ************************************** 	
 
@@ -54,6 +65,7 @@ nosliw.registerSetNodeDataEvent("common.segmentparser.parseSegment", function(){
 nosliw.registerSetNodeDataEvent("request.request.createServiceRequestInfoSequence", function(){	node_createServiceRequestInfoSequence = this.getData();	});
 nosliw.registerSetNodeDataEvent("common.service.ServiceInfo", function(){node_ServiceInfo = this.getData();	});
 nosliw.registerSetNodeDataEvent("expression.entity.OperationParm", function(){node_OperationParm = this.getData();});
+nosliw.registerSetNodeDataEvent("uidata.data.utility", function(){node_dataUtility = this.getData();});
 
 //Register Node by Name
 packageObj.createChildNode("utility", node_utility); 
