084729d5e1d5e6467e206b8d459dc7566430d39e
ning wang <forbestwn@hotmail.com>
2023-05-27 00:24:42 -0400

    1111111

--- Application/src/main/java/com/nosliw/servlet/app/browsecomplexentity/HAPBrowseComplexEntityServlet.java
+++ Application/src/main/java/com/nosliw/servlet/app/browsecomplexentity/HAPBrowseComplexEntityServlet.java
@@ -2,6 +2,7 @@ package com.nosliw.servlet.app.browsecomplexentity;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -55,12 +56,15 @@ public class HAPBrowseComplexEntityServlet extends HAPServiceServlet{
 	private List<HAPContainerComplexEntity> buildComplexEntityContainer(HAPRuntimeEnvironmentImpBrowser runtimeEnv){
 		List<HAPContainerComplexEntity> out = new ArrayList<HAPContainerComplexEntity>();
 		
-		Set<String> complexEntityTypes = runtimeEnv.getDomainEntityExecutableManager().getComplexEntityTypes();
+		List<String> complexEntityTypes = new ArrayList<String>();
+		complexEntityTypes.addAll(runtimeEnv.getDomainEntityExecutableManager().getComplexEntityTypes());
+		Collections.sort(complexEntityTypes);
 		for(String complexEntityType : complexEntityTypes) {
 			HAPContainerComplexEntity entityContainer = new HAPContainerComplexEntity(complexEntityType);
 			
 			String basePath = HAPSystemFolderUtility.getResourceFolder(complexEntityType);
 			
+			List<String> resourceIds = new ArrayList<String>();
 			Set<File> childrenFile = HAPUtilityFile.getChildren(basePath);
 			for(File childFile : childrenFile) {
 				String resourceId = null;
@@ -68,8 +72,14 @@ public class HAPBrowseComplexEntityServlet extends HAPServiceServlet{
 					resourceId = childFile.getName();
 				}
 				else {
-					resourceId = childFile.getName();
+					resourceId = HAPUtilityFile.getFileCoreName(childFile.getName());
 				}
+				resourceIds.add(resourceId);
+			}
+			
+			Collections.sort(resourceIds);
+			
+			for(String resourceId : resourceIds) {
 				entityContainer.addComplexEntityInfo(new HAPInfoComplexEntity(new HAPResourceIdSimple(complexEntityType, resourceId)));
 			}
 			
@@ -84,7 +94,8 @@ public class HAPBrowseComplexEntityServlet extends HAPServiceServlet{
 		
 		String basePath = HAPSystemFolderUtility.getResourceFolder(HAPConstantShared.RUNTIME_RESOURCE_TYPE_CONFIGURE);
 		Set<File> childrenFile = HAPUtilityFile.getChildren(basePath);
-		for(File childFile : childrenFile) {
+		List<File> childrenFileSorted = HAPUtilityFile.sortFiles(childrenFile);
+		for(File childFile : childrenFileSorted) {
 			if(!childFile.isDirectory()) {
 				out.add(HAPUtilityFile.getFileCoreName(childFile.getName()));
 			}
