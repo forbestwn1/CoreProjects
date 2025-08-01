cc17b89b40f0100a84249b1f4bb098059e68c837
forbestwn1 <forbestwn@gmail.com>
2025-07-31 23:49:37 -0400

    1111111

---
+++ Core/src/com/nosliw/core/resource/HAPRuntimeTaskLoadResources.java
@@ -0,0 +1,22 @@
+package com.nosliw.core.resource;
+
+import java.util.List;
+
+import com.nosliw.core.runtime.HAPRuntimeTask;
+
+public abstract class HAPRuntimeTaskLoadResources extends HAPRuntimeTask{
+
+	final public static String TASK = "LoadResources"; 
+
+	private List<HAPResourceInfo> m_resourcesInfo;
+	
+	public HAPRuntimeTaskLoadResources(List<HAPResourceInfo> resourcesInfo){
+		this.m_resourcesInfo = resourcesInfo;
+	}
+	
+	public List<HAPResourceInfo> getResourcesInfo(){		return this.m_resourcesInfo;	}
+	
+	@Override
+	public String getTaskType(){  return TASK; }
+
+}
